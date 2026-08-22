f28ccd944e30a4eb9c7ea355be3acaca79bd7bac
Briyit <lisetrodriguezastros@gmail.com>
2026-08-22 20:34:20 +0200

    enviar numero de telefono a la plataforma , eliminando el null de el schema y model

--- backend/app/schemas/contact.py
+++ backend/app/schemas/contact.py
@@ -5,7 +5,7 @@ from pydantic import ConfigDict
 class ContactCreate(BaseModel):
     nombre: str = Field(min_length=3, max_length=50)
     email: EmailStr
-    telefono: str | None = Field(default=None, pattern=r"^\+?\d{9,15}$")
+    telefono: str = Field(pattern=r"^\+?\d{9,15}$")
     mensaje: str = Field(min_length=5, max_length=500)
     tipo: str = "general"
 
